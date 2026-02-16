440c1c9283e2c548731136896d3a17fd1187fcd1
Raphael del <officielraphaeldelhomenede@gmail.com>
2026-02-16 19:12:24 +0100

    Create index.php

---
+++ TNSI/ancienne-version/1NSI/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; url=https://tifsec.github.io/1NSI">
+    <title>Redirection...</title>
+</head>
+<body>
+    <p>Si vous n'êtes pas redirigé, <a href="https://VOTRE-NOUVELLE-URL.com/">c'est que le HTML est désactivé sur ton navigateur et c'est triste</a>.</p>
+</body>
+</html>
