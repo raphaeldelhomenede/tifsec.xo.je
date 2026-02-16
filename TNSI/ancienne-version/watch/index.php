0692b64e32bdc03d3e7df767faac9509b9fc0193
Raphael del <officielraphaeldelhomenede@gmail.com>
2026-02-16 17:22:06 +0100

    Add files via upload

--- TNSI/ancienne-version/watch/index.php
+++ TNSI/ancienne-version/watch/index.php
@@ -1,1 +1,12 @@
+<?php
+$redirects = json_decode('{
+    "lxrLR1LoX6E": "https://is.gd/YS6zts",
+    "ritRJJLiVLc": "https://is.gd/WJqb4P",
+    "dzzB9pbXNKE": "https://is.gd/CgaVcx"
+}', true);
 
+if (isset($_GET["v"]) && array_key_exists($_GET["v"], $redirects)) { ?>
+    <meta http-equiv="refresh" content="0; URL=<?= $redirects[$_GET["v"]] ?>" /><?php
+    exit();
+}
+?>
