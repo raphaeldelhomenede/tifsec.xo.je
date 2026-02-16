50a3d248beb93f5dc9779f00389227bbe782611d
Raphael del <officielraphaeldelhomenede@gmail.com>
2026-02-16 19:11:18 +0100

    Create index.php

---
+++ TNSI/ancienne-version/SNT/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; url=https://tifsec.github.io/SNT">
+    <title>Redirection...</title>
+</head>
+<body>
+    <p>Si vous n'êtes pas redirigé, <a href="https://VOTRE-NOUVELLE-URL.com/">c'est que tu n'as pas activé ton HTML sur navigateur et ca c'est dommage.</a>.</p>
+</body>
+</html>
